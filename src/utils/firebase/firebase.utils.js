f82ccdbe7f0fe43a1b1ebc50a6838438c0550d91
Jacob White <jakerobertwhite@gmail.com>
2023-05-21 00:38:38 -0400

    creating database
    created collections in firebase and written functionality into firebase.utils

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,9 @@ import {
         GoogleAuthProvider //signInWithRedirect
             } from 'firebase/auth';
 
-
+import { 
+    getFirestore, doc, getDoc, setDoc
+ } from 'firebase/firestore';
 
 
 const firebaseConfig = {
@@ -29,4 +31,35 @@ provider.setCustomParameters({
 export const auth = getAuth();
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async(userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef)
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+
+    //if user data exists...then return userDocRef
+    //if user data does not exist...then set into my collection
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch(error){
+            console.log('error creating user', error.message)
+        }
+    }
+    return userDocRef;
+}
 
