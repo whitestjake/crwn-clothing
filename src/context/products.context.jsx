b6211f6decdae5b52dcbdf49bd8439aad653ad3c
Jacob White <jakerobertwhite@gmail.com>
2023-06-01 02:48:40 -0400

    updated cart drop menu
    enabled add to car functionality

--- src/context/products.context.jsx
+++ src/context/products.context.jsx
@@ -3,11 +3,10 @@ import { createContext, useState } from "react";
 
 import PRODUCTS from '../shop-data.json';
 
-
 export const ProductsContext = createContext({});
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products] = useState(PRODUCTS);
     const value = ({ products });
 
     return (
