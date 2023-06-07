b0f8695f5d75e8b33f9c6b39c76dde1ad81cc84d
Jacob White <jakerobertwhite@gmail.com>
2023-06-07 02:24:22 -0400

    updated shop page
    seperated shop page by previewable categories, updated database to contain preset items for building shop functionality

--- src/App.js
+++ src/App.js
@@ -11,16 +11,13 @@ const App = () => {
 
   return (
     <Routes>
-      
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
-    
     </Routes> 
-
   )
 }
 
