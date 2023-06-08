75b611594ffde0e9194287ebeb5a6b9cc1816a5f
Jacob White <jakerobertwhite@gmail.com>
2023-06-08 02:22:21 -0400

    fixed scss issues
    fixed overlapping scss attributes

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,4 +1,6 @@
 
+import { Link } from 'react-router-dom';
+
 import ProductCard from '../product-card/product-card.component';
 
 import './category-preview.styles.scss';
@@ -9,7 +11,7 @@ const CategoryPreview = ({ title, products }) => {
     return (
     <div className='category-preview-container'>
         <h2>
-            <span className='title'>{title.toUpperCase()}</span>
+            <Link className='title' to={title}>{title.toUpperCase()}</Link>
         </h2>
         <div className='preview'>
             {
--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,16 +1,16 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
     const { imageUrl, title } = category;
     return (
 
-        <div className='category-container'>
+        <div className='directory-item-container'>
           <div 
             className='background-image' style={{
               backgroundImage: `url(${imageUrl})`
             }}
           /> 
-          <div className='category-body-container'>
+          <div className='body'>
             <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
