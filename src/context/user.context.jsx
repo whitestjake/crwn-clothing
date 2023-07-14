70b81ccdaf3cfcd88b3146c71106ccbaf7d5b3d3
Jacob White <jakerobertwhite@gmail.com>
2023-07-14 08:01:11 -0400

    userReducer
    implemented useReducer instead of useState for user context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,5 +1,5 @@
 
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import { 
     createUserDocumentFromAuth,
@@ -13,9 +13,38 @@ export const UserContext = createContext({
     currentUser: null
 })
 
+
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+    
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+    }
+
     const value = { currentUser, setCurrentUser }
 
     useEffect(() => {
@@ -37,7 +66,15 @@ export const UserProvider = ({ children }) => {
 }
 
 
+/* 
 
+const userReducer = (state, action) => {
+    return {
+        currentUser:
+    }
+}
+
+*/
 
 
 
