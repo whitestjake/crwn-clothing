2f462adc565011d8a74327b82b35422cf16acc3c
Jacob White <jakerobertwhite@gmail.com>
2023-08-12 05:33:32 -0400

    Update category.selector.js

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,8 +1,11 @@
 
 
-export const selectCategoriesMap = (state) => 
-state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-}, {});
+export const selectCategoriesMap = (state) => {
+
+    return state.categories.categories.reduce((acc, category) => {
+        const { title, items } = category;
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {})
+
+}
