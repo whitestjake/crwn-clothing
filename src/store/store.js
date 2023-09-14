b41a73daa0e387388d557f5486e8d1af8a4691d2
Jacob White <jakerobertwhite@gmail.com>
2023-09-14 07:34:13 -0400

    redux migration
    migrating category context to redux

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,22 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
+
+
+// const loggerMiddleWare = (store) => (next) => (action) => {
+//     if(!action.type) {
+//         return next(action);
+//     }
+
+//     console.log('type', action.type);
+//     console.log('payload', action.payload);
+//     console.log('currentState: ', store.getState());
+
+//     next(action);
+
+//     console.log('next state: ', store.getState())
+// }
+
 const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
