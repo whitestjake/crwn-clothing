064307612eb3b908eea528059106738f4e0e16e7
Jacob White <jakerobertwhite@gmail.com>
2023-10-03 20:35:47 -0400

    console.log
    removing console logs that i did not mean to leave in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -24,7 +24,7 @@ useEffect(() => {
       }
       const pickedUser = 
         user && (({accessToken, email}) => ({ accessToken, email })(user));
-      console.log(setCurrentUser(pickedUser))
+      // console.log(setCurrentUser(pickedUser))
       dispatch(setCurrentUser(pickedUser));
   });
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
 import { store } from './store/store';
